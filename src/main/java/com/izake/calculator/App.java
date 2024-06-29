6b402b4b63f642402bced401d101097c06571b30
124k3 <akha3743@gmail.com>
2024-06-29 03:57:37 -0700

    initial commit

---
+++ src/main/java/com/izake/calculator/App.java
@@ -0,0 +1,10 @@
+package com.izake.calculator;
+
+import com.izake.calculator.ui.CalculatorUI;
+
+public class App {
+
+    public static void main(String[] args) {
+        new CalculatorUI();
+    }
+}
